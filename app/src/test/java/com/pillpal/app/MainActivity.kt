992dfe3f09f1c57b4935d6ea176c8e869232e751
X14RLZ <charleschantioco@gmail.com>
2023-11-14 13:34:17 +0800

    updated the calendar empty page

--- app/src/test/java/com/pillpal/app/MainActivity.kt
+++ app/src/test/java/com/pillpal/app/MainActivity.kt
@@ -6,26 +6,27 @@ import com.pillpal.app.databinding.LayoutProgressDialogBinding
 class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
 
     /*
-    NOTE SURE DITO DI PA NAMAN NEDETECT yung Day Scroll Date Picker, tas Java kase gamit nung nag-basehan ko hehe
+    NOTE SURE DITO DI NAMAN NEDETECT yung Day Scroll Date Picker, tas Java kase gamit nung nag-basehan ko hehe
 
     private lateinit var dayDatePicker: DayScrollDatePicker
-    private
-    var selectedDate: String? = null
-    override
-    fun onCreate(savedInstanceState: Bundle?) {
+    private var selectedDate: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         dayDatePicker = findViewById(R.id.dayDatepicker)
-        dayDatePicker.startDate = Date(2023, 11, 14)
+        dayDatePicker.setStartDate(14, 11, 2023)
 
-        dayDatePicker.selectedDateListener = object : DayScrollDatePicker.UnDateSelectedListener {
-            override fun onDateSelected(date: Date?) {
+        dayDatePicker.setOnDateSelectedListener(object : UnDateSelectedListener {
+            override fun onDateSelected(@Nullable date: Date?) {
                 selectedDate = date?.toString()
                 Toast.makeText(this@MainActivity, selectedDate, Toast.LENGTH_SHORT).show()
             }
-        }
-    }*/
+        })
+    }
+
+    */
 
     override fun onInitialized() {
 
